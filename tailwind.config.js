18d31ebde169a411c6c06aa82c7e02236cdd3f58
Daniel Wentsch <hello@wentsch.me>
2020-07-16 22:45:16 +0200

    Fill container with copy of items until full

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+            padding: {
+                default: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+              },
+          },
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+}
